90c16e742d4486d27443b0742bec99fb7ab77748
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-02-21 16:47:30 +0200

    Added hero landing page

--- frontend/src/components/HeroLayout.tsx
+++ frontend/src/components/HeroLayout.tsx
@@ -2,8 +2,11 @@ import hero from '../assets/BurgerHero.jpg'
 
 const HeroLayout = () => {
     return (
-        <div>
-            <img src={hero} className='w-full max-h-fit object-cover'/>
+        <div className='flex justify-between gap-8'>
+            <div className='flex: 50% flex-1 justify-center items-center'>
+                Takeaway today
+            </div>
+            <img src={hero} className='flex: 50% flex-1 max-w-[50%] max-h-[50%]' />
         </div>
     )
 }
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+
+const HomePage = () => {
+    return (
+        <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-5 text-center -mt-16 bg-white rounded-lg shadow-md py-6">
+                <h1 className="text-5xl font-bold tracking-tight text-orange-500">
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
