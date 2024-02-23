1807e1f0c85c8ae89a741d9d84c7fc7ac2d6e593
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-02-23 16:38:57 +0200

    Added user menu for logged users

--- frontend/src/components/NavMain.tsx
+++ frontend/src/components/NavMain.tsx
@@ -1,20 +1,28 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from './ui/button';
+import UserNavMenu from './UserNavMenu';
 
 const NavMain = () => {
-    const { loginWithRedirect } = useAuth0();
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
 
     const loginRedirect = async () => {
         await loginWithRedirect();
     };
 
     return (
-        <Button
-            onClick={loginRedirect}
-            variant="ghost"
-            className='font-bold text-orange-400 hover:text-orange-600 hover:bg-white text-lg'>
-            Log In
-        </Button>
+        <span className='flex space-x-2 items-center'>
+            {isAuthenticated ? (
+                <UserNavMenu />
+            ) : (
+                <Button
+                    onClick={loginRedirect}
+                    variant="ghost"
+                    className='font-bold text-orange-400 hover:text-orange-600 hover:bg-white text-lg'>
+                    Log In
+                </Button>
+            )}
+        </span>
+
     )
 };
 
