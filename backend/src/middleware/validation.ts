f9a8ffecee5e673b27a7c2aecdd8526293641f7f
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-02-24 19:09:27 +0200

    Added error validation for user profile

--- backend/src/middleware/validation.ts
+++ backend/src/middleware/validation.ts
@@ -1,8 +1,32 @@
-import { body } from "express-validator";
+import { NextFunction, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+
+const validationErrorHandler = (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+    next();
+};
 
 export const validateUserRequest = [
-    body("name").isString().notEmpty().withMessage("Name must be a string"),
-    body("address").isString().notEmpty().withMessage("Address must be a string"),
-    body("city").isString().notEmpty().withMessage("City must be a string"),
-    body("country").isString().notEmpty().withMessage("Country must be a string"),
+    body("name")
+        .isString()
+        .notEmpty()
+        .withMessage("Name must be a string"),
+    body("address")
+        .isString()
+        .notEmpty()
+        .withMessage("Address must be a string"),
+    body("city")
+        .isString()
+        .notEmpty()
+        .withMessage("City must be a string"),
+    body("country")
+        .isString()
+        .notEmpty()
+        .withMessage("Country must be a string"),
+    validationErrorHandler
 ];
--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import userController from "../controllers/userController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
 router.post("/", jwtCheck, userController.createUser);
-router.put("/", jwtCheck, jwtParse, userController.updateUser);
+router.put("/", jwtCheck, jwtParse, validateUserRequest, userController.updateUser);
 
 export default router;
