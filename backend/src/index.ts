d28f8ffc152b60f6978dbef9238cfa50f295baae
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-02-27 15:03:32 +0200

    Added cloudinary for image uploading

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,16 @@ import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import userRoute from './routes/userRoute';
+import { v2 as cloudinary } from 'cloudinary'
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
     .then(() => console.log('Connected to MongoDB'));
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const app = express();
 app.use(express.json());
