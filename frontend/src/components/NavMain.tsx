d243fd6a80cb96b00db3090d682a9a1045e6e5a5
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2024-05-11 18:24:00 +0300

    Re

--- frontend/src/components/NavMain.tsx
+++ frontend/src/components/NavMain.tsx
@@ -1,7 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from './ui/button';
 import UserNavMenu from './UserNavMenu';
-import { Link } from 'react-router-dom';
 
 const NavMain = () => {
     const { loginWithRedirect, isAuthenticated } = useAuth0();
